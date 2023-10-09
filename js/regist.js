9cd539f2866a2277eeb24a790cebc26f672c06d1
putricantik <putridwi1676@gmail.com>
2023-10-10 00:42:30 +0700

    finalisasi fix

--- js/regist.js
+++ js/regist.js
@@ -1,49 +1,42 @@
 const form = document.querySelector('form')
 
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-
-    // mendapatkan data dari inputan form
-    // let name = document.querySelector('.name').value
-
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const number = document.getElementById('number');
     const courses = document.getElementById('courses');
-    const gender = document.getElementById('gender');
     
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     // Validasi formulir
-    if (!name.checkValidity() || !email.checkValidity() || !number.checkValidity() || !courses.checkValidity()  || !gender.checkValidity()) {
-        // Validasi gagal, tampilkan pesan atau lakukan sesuatu
-        alert('Silakan isi semua field dengan benar.');
-        return; // Hentikan proses submit formulir
-    }
+    // if (!name.checkValidity() || !email.checkValidity() || !number.checkValidity() || !courses.checkValidity()  || !gender.checkValidity()) {
+    //     // Validasi gagal, tampilkan pesan atau lakukan sesuatu
+    //     alert('Silakan isi semua field dengan benar.');
+    //     return; // Hentikan proses submit formulir
+    // }
+
+    const gender = document.querySelector('input[name="gender"]:checked')
 
     const data = {
         name: name.value,
         email: email.value,
-        number: number.value,
-        courses: courses.value,
+        number: parseInt(number.value),
+        course: courses.value,
         gender: gender.value 
     }
+
+    console.log(data);
 
     fetch('https://be-balikpapan-13-production.up.railway.app/registration', {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
     .then(response => response.json())
     .then(data => {
-        if (data.message === 'Success POST/CREATE Peserta') {
-            // Daftar berhasil, alert pesan
-            alert('S!');
-        } else {
-            // Daftar gagal atau respons tidak sesuai
-            console.error('Daftar Course Failed:', data.message);
-        }
+        alert('Selamat Pesan Anda Berhasil Terkirim');
+        form.reset();
     })
     .catch(error => {
         console.error('Error:', error);
